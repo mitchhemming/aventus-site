a8a17ee0aff9db1119e56451b62467563c2c80fa
mitchhemming <mitch@mhemming.com>
2026-04-20 16:08:27 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,11 +13,12 @@
   let lenis = null;
   if (typeof Lenis !== 'undefined') {
     lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      duration: 0.8,
+      easing: (t) => 1 - Math.pow(1 - t, 3),
       smoothWheel: true,
-      wheelMultiplier: 1,
+      wheelMultiplier: 1.2,
       touchMultiplier: 2,
+      lerp: 0.1,
     });
     lenis.on('scroll', ScrollTrigger.update);
     gsap.ticker.add((time) => lenis.raf(time * 1000));
